cdf28aae20373f0742c2aa690c694f55456b781d
CesarSP99 <cesarandres00@gmail.com>
2021-03-16 19:12:25 -0400

    Agregado Modelo

---
+++ ServerSide Parcial 1/ApiDoble/Models/Random.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiDoble.Models
+{
+    public class Random
+    {
+        [Key]
+        [DataType(DataType.DateTime)]
+        public DateTime DateAndTime { get; set; }
+        [Required]
+        public int RandomNumber { get; set; }
+    }
+}
